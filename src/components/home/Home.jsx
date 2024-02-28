b9bc22fc8b661afac64b1ab1c9eb5b6c6d3a33aa
Jachen Liu <jachenliu2017@gmail.com>
2024-02-28 18:56:11 -0500

    added svgs and home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css';
+import React from 'react';
+
+function Home() {
+    return (
+        <>
+            <div className="home-container">
+                <h1 className="home-text">
+                    Hello welcome to my home page.
+                </h1>
+            </div>
+        </>
+    )
+}
+export default Home;

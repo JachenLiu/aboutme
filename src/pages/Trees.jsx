6cb99fadbf295b685728961b873aa0db46214481
JachenLiu <38360804+JachenLiu@users.noreply.github.com>
2024-12-11 20:01:31 -0500

    figuring out deployment

--- src/pages/Trees.jsx
+++ src/pages/Trees.jsx
@@ -4,7 +4,10 @@ import '../styles/Trees.css';
 export function Trees() {
   return (
     <div className='trees'>
-        <iframe src="/trees_density.html"width={1000} height={500}></iframe>
+      <a href="/trees_density.html" >
+          Trees
+        </a>
+        {/* <iframe src="/trees_density.html"width={1000} height={500}></iframe> */}
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ const repoName = 'aboutme';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/`,
+  base: `./}`,
 })

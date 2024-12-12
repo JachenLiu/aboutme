2cafcb9348a010204749a5906da64ba34ff4ba1b
JachenLiu <38360804+JachenLiu@users.noreply.github.com>
2024-12-11 20:44:50 -0500

    set base config
    changed to url

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'aboutme';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}`,
+  base: `https://jachenliu.github.io/aboutme/`,
 })

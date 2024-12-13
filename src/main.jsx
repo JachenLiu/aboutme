790aa4f594c230bbdcc60c2605c1701cff7c4fa4
JachenLiu <38360804+JachenLiu@users.noreply.github.com>
2024-12-13 00:23:30 -0500

    changed repo name

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/aboutme">
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </StrictMode>,
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export function About() {
     <div className="about">
       <h1>About Me</h1>
       <p> First thing I wanted to do was make a light and dark mode.</p>
-      <p> Working on my game, then I'll add the WebGL build to this site so you can play it here.</p>
+      <p> Enjoy the flashbang</p>
     </div>
   );
 }
